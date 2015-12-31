61641b8b8354eddf3dff430e9f8ce18bcfce1740
ajmssc <jeanmarc.soumet@gmail.com>
2015-12-30 17:32:41 -0800

    first commit

---
+++ src/ratpack/Ratpack.groovy
@@ -0,0 +1,22 @@
+import ratpack.groovy.template.MarkupTemplateModule
+
+import static ratpack.groovy.Groovy.groovyMarkupTemplate
+import static ratpack.groovy.Groovy.ratpack
+
+ratpack {
+  bindings {
+    module MarkupTemplateModule
+  }
+
+  handlers {
+    get {
+      render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
+    }
+
+    get("test/:name") {
+      render "Hello $pathTokens.name!"
+    }
+
+    files { dir "public" }
+  }
+}
